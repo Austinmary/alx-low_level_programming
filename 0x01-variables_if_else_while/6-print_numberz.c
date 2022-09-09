5c896576eeef939b9b0d2083951c84c7b0cef6d0
Augustinemary Agupugo <austinmary456@gmail.com>
2022-09-09 16:17:28 -0700

    A program that prints all single digit numbers of base 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,11 +3,12 @@
 
 int main()
 {
-    int n = 0;
-    while (n <= 9){
-        putchar(n + '0');
-        ++n;
-    }
-    putchar('\n');
-    return 0;
+int n = 0;
+while (n <= 9)
+{
+putchar(n + '0');
+++n;
+}
+putchar('\n');
+return 0;
 }
