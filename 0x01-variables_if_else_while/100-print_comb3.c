4c7f1bc9463793f0259c982b36a6aa3b8365fa70
Augustinemary Agupugo <austinmary456@gmail.com>
2022-09-09 18:30:44 -0700

    A program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,35 +1,39 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- *main - program that prints the numbers from 01 to 89
- *Numbers must be separated by , followed by a space
- *Numbers should be printed in ascending order, with 2 digits
- *You can only use the putchar function
-Return: 0
+*
+*
+* Description: print
+*
+*
+*
 */
-
 int main(void)
 {
-int n1 = 48;
-int n2;
-int com = 44;
+    int n1 = 0, n2;
 
-while (n1 <= '57')
-{
-n2 = n1 + 1;
-while (n2 <= '57')
-{
-putchar(n1);
-putchar(n2);
-if (n1 != 56 || n2 != 57)
-{	
-putchar(com);
-putchar(32);
+    while(n1 <= 9)
+    {
+        n2 = 0;
+        while(n2 <= 9)
+        {
+            if(n1 != n2 && n1 < n2)
+        {
+            putchar(n1 + 48);
+            putchar(n2 + 48);
+
+            if(n1 + n2 != 17)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+        ++n2;
+        }
+         ++n1;
+    }
+
+    putchar('\n');
+    return 0;
 }
-n2 <= 1;
-}
-n1 <= 1;
-}
-putchar('\n');
-return (0);
-}
+
