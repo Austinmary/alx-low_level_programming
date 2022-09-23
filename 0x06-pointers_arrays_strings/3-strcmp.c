f751dcaf5af7948f3e72315f61a4eeffeecbb832
Augustinemary Agupugo <austinmary456@gmail.com>
2022-09-22 18:58:41 -0700

    A function that prints two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,15 +7,16 @@
  *
  * Return: If str1 < str2, the negative difference of the first unmatched characters.
  * If s1 == s2, 0.
- * If s1 > s2, the positive difference of the first unmatched characters.
+ * If s1 > s2, the positive difference
  */
 
 int _strcmp(char *s1, char *s2)
+
 {
+
 	while (*s1 && *s2 && *s1 == *s2)
 	{
 		s1++;
-
 		s2++;
 	}
 
