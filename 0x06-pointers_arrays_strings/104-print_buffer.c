8c39dc93b920a8b9afec0f797eae9925bf75a77f
Augustinemary Agupugo <austinmary456@gmail.com>
2022-09-22 17:41:20 -0700

    A function that prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -14,18 +14,19 @@ int m, n;
 
 for (m = 0; m <= 9; m++)
 {
-if (m <= s)
-printf("%02x", c[l * 10 + m]);
-else
-printf("  ");
-if (m % 2)                                        putchar(' ');
+	if (m <= s)
+	printf("%02x", c[l * 10 + m]);
+	else
+	printf("  ");
+	if (m % 2)
+	putchar(' ');
 }
 for (n = 0; n <= s; n++)
 {
-if (c[l * 10 + n] > 31 && c[l * 10 + n] < 127)
-putchar(c[l * 10 + n]);
-else
-putchar('.');
+	if (c[l * 10 + n] > 31 && c[l * 10 + n] < 127)
+	putchar(c[l * 10 + n]);
+	else
+	putchar('.');
 }
 }
 
@@ -43,15 +44,17 @@ int i;
 
 for (i = 0; i <= (size - 1) / 10 && size; i++)
 {
-printf("%08x: ", i * 10);
-if (i < size / 10)                                {
-print_line(b, 9, i);
+	printf("%08x: ", i * 10);
+	if (i < size / 10)
+{
+	print_line(b, 9, i);
 }
 else
 {
-print_line(b, size % 10 - 1, i);
+	print_line(b, size % 10 - 1, i);
 }
-putchar('\n');                                    }
-if (size == 0)
 putchar('\n');
+}
+if (size == 0)
+	putchar('\n');
 }
