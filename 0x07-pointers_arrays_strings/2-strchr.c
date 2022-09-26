421b13770508f3235f3b1790732b6928a360d2c3
Augustinemary Agupugo <austinmary456@gmail.com>
2022-09-26 14:23:21 -0700

    A function of strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,10 +10,11 @@
 char *_strchr(char *s, char c)
 {
 	int a;
-	
+
 	for (a = 0; *(s + a); a++)
 	{
-		if (*(s + a) == c)			return (s + a);
+		if (*(s + a) == c)
+			return (s + a);
 	}
 	if (*(s + a) == c)
 		return (s + a);
