6dc8401f5dba3d0289fcd85150f7e8a734265c71
Augustinemary Agupugo <austinmary456@gmail.com>
2022-09-28 17:22:53 -0700

    A function that compares two strings and returns 1 if the strings can be considered identical

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,18 +1,57 @@
 #include "main.h"
 
-int strlen_no_wilds(char *str);
-void iterate_wild(char **wildstr);
-char *postfix_match(char *str, char *postfix);
-int wildcmp(char *s1, char *s2);
+int check_wildcmp(char *s);
 
 /**
- * strlen_no_wilds - Returns the length of a string, ignoring wildcard characters
- * @str: The string to be measured
+ * wildcmp - Returns if a string is wildcmp
+ * @s: the string value to be checked
  *
- * Return: The length
+ * Return: integer value
  */
-
-int strlen_no_wilds(char *str)
+int wildcmp(char *s)
 {
-	int len = 0, index = 0;
+if (*s == '0')
+return (1);
+
+return (check_wildcmp(s));
+}
+
+/**
+ * check_wildcmp - check if a string is wildcmp
+ * @s: the string value to be checked
+ * Return: integer value
+ */
+int check_wildcmp(char *s)
+{
+int l = _strlen_recursion(s) - 1;
+
+
+if (*s == s[l])
+{
+s++;
+l--;
+}
+else
+{
+return (0);
+}
+
+return (1);
+}
+
+/**
+ * _strlen_recursion - Get the length of a string
+ * @s: the string to get the length
+ *
+ * Return: the string length
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
+
+s++;
+return (_strlen_recursion(s) + 1);
 }
