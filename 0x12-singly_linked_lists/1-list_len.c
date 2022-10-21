6c1b98570915180310c494bbccfa91a9abd24646
Augustinemary Agupugo <austinmary456@gmail.com>
2022-10-20 20:53:55 -0700

    Singly Functions

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,20 +4,21 @@
 #include "lists.h"
 
 /**
- * list_len - returns then number of elements in a list.
- * @h: singly linked list.
- * Return: number of elements in the list.
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: head of linked list
+ * Return: number of elements
  */
 
 size_t list_len(const list_t *h)
 {
-    size_t nelem;
+	size_t size;
 
-	nelem = 0;
-    while (h != NULL)
-    {
-        h = h->next;
-        nelem++;
-    }
-    return (nelem);
+	size = 0;
+	while (h != NULL)
+	{
+		size++;
+		h = h->next;
+	}
+
+	return (size);
 }
