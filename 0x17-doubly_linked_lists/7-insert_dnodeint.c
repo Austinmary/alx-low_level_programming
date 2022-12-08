f797e06f7cccde79dbd12d7f80ccf605d8e76e67
Augustinemary Agupugo <austinmary456@gmail.com>
2022-12-08 14:48:35 -0800

    Doubly linked lists C algorithm data structure

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,39 +11,32 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	/* declarations */
-	dlistint_t *location;
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *location, *new = *h;
+	/* unsigned int counter = 0 */
 
-	/* check for NULL */
-	if (!h || !new)
-		return (new ? free(new), NULL : NULL);
-	/* assign some things to new */
-	location = n;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
+	{
+		if (location == NULL)
+			return (NULL);
+		location = location->next;
+	}
+
+	if (location->next == NULL)
+		return (add_dndeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
 	new->n = n;
-	/* if index is 0 */
-	if (!idx)
-	{
-		new->prev = NULL;
-		new->next = location ? location : NULL;
-		if (location)
-		{
-			location->prev = new;
-		}
-		return (*h = new);
-	}
-	/* otherwise, move to place before index and install new node */
-	for (; location; location = location->next, idx--)
-	{
-		if (idx - 1 == 0)
-		{
-			new->prev = location;
-			new->next = location->next;
-			if (new->next)
-				new->next->prev = new;
-			location->next = new;
-			return (new);
-		}
-	}
-	/* if all else fails, free new & retrun NULL */
-	return (free(new), NULL);
+	new->next = location->next;
+	new->prev = location;
+	location->next->prev = new;
+	location->next = new;
+
+	return (new);
 }
