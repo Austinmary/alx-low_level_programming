ec15bf3bd8af9995b17ee89861eb509d32342e62
Augustinemary Agupugo <austinmary456@gmail.com>
2022-12-08 15:34:13 -0800

    Doubly linked lists C algorithm data structure

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -39,3 +39,65 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	return (new_mem);
 }
+
+#include "lists.h"
+/**
+ * add_dnodeint- Insert new node at the beginnig
+ * @head: Is the pointer to the direction of the head
+ * @n:The data integer
+ * Return: The new node
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_mem;
+
+	new_mem = malloc(sizeof(dlistint_t));
+	if (new_mem == NULL)
+		return (NULL);
+
+	new_mem->n = n;
+	new_mem->prev = NULL;
+	new_mem->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = new_mem;
+	*head = new_mem;
+
+	return (new_mem);
+}
+
+#include "lists.h"
+/**
+ * add_dnodeint_end - Add a node in the tail
+ * @head: Pointer to direction of the head
+ * @n: The data integer
+ * Return: The direction of the tail node
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_mem, *last_dir;
+
+	new_mem = malloc(sizeof(dlistint_t));
+
+	if (new_mem == NULL)
+		return (NULL);
+
+	new_mem->n = n;
+	new_mem->next = NULL;
+	new_mem->prev = NULL;
+	if (*head == NULL)
+	{
+		*head = new_mem;
+		return (new_mem);
+	}
+	last_dir = *head;
+
+	while (last_dir->next != NULL)
+		last_dir = last_dir->next;
+	last_dir->next = new_mem;
+	new_mem->prev = last_dir;
+
+	return (new_mem);
+}
