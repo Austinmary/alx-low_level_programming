18c839f74ea7dc77ecc64898895d01c515746392
Augustinemary Agupugo <austinmary456@gmail.com>
2022-12-08 15:59:41 -0800

    Doubly linked lists C algorithm data structure

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,103 +1,54 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Insert a node in the postion
- * @h: Pointer to direction of the head
- * @n: The data integer
- * @idx: Position at the insert the new node
- * Return: The direction of the new node
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_mem, *res_mem = *h;
-	/* unsigned int counter = 0 */
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
+	new = NULL;
 	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	for (; idx != 1; idx--)
+		new = add_dnodeint(h, n);
+	else
 	{
-		if (res_mem == NULL)
-			return (NULL);
-		res_mem = res_mem->next;
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
 	}
 
-	if (res_mem->next == NULL)
-		return (add_dndeint_end(h, n));
-
-	new_mem = malloc(sizeof(dlistint_t));
-
-	if (new_mem == NULL)
-		return (NULL);
-
-	new_mem->n = n;
-	new_mem->next = res_mem->next;
-	new_mem->prev = res_mem;
-	res_mem->next->prev = new_mem;
-	res_mem->next = new_mem;
-
-	return (new_mem);
-}
-
-#include "lists.h"
-/**
- * add_dnodeint- Insert new node at the beginnig
- * @head: Is the pointer to the direction of the head
- * @n:The data integer
- * Return: The new node
- */
-
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-{
-	dlistint_t *new_mem;
-
-	new_mem = malloc(sizeof(dlistint_t));
-	if (new_mem == NULL)
-		return (NULL);
-
-	new_mem->n = n;
-	new_mem->prev = NULL;
-	new_mem->next = *head;
-
-	if (*head != NULL)
-		(*head)->prev = new_mem;
-	*head = new_mem;
-
-	return (new_mem);
-}
-
-#include "lists.h"
-/**
- * add_dnodeint_end - Add a node in the tail
- * @head: Pointer to direction of the head
- * @n: The data integer
- * Return: The direction of the tail node
- */
-
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
-{
-	dlistint_t *new_mem, *last_dir;
-
-	new_mem = malloc(sizeof(dlistint_t));
-
-	if (new_mem == NULL)
-		return (NULL);
-
-	new_mem->n = n;
-	new_mem->next = NULL;
-	new_mem->prev = NULL;
-	if (*head == NULL)
-	{
-		*head = new_mem;
-		return (new_mem);
-	}
-	last_dir = *head;
-
-	while (last_dir->next != NULL)
-		last_dir = last_dir->next;
-	last_dir->next = new_mem;
-	new_mem->prev = last_dir;
-
-	return (new_mem);
+	return (new);
 }
