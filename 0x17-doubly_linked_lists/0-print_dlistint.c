8d7b2ccbc07d6baf5c5fe3386931b50bd420ddee
Augustinemary Agupugo <austinmary456@gmail.com>
2022-12-08 14:28:25 -0800

    Doubly linked lists C algorithm data structure

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,19 +11,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
 
-	if (h == 0)
+	while (h != NULL)
 	{
-		printf("Empty list");
-
-	}
-	else
-	{
-		while (h)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			i++;
-		}
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
 	}
 	return (i);
 }
